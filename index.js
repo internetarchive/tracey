55ed95c4ce9d4e9cc95e090ca3181ba062cdbf31
tracey <tracey@archive.org>
2020-08-06 15:38:20 -0700

    Demuxed talk added.  now can link to talk or slides

--- index.js
+++ index.js
@@ -19,7 +19,8 @@ const ME = {
     slug: 'kubecon',
     title: 'KubeCon 2018 - Seattle',
     img: 'kubecon.jpg',
-    descrip: 'Migrating Internet Archive to Kubernetes',
+    descrip: `Migrating Internet Archive to Kubernetes
+      <br><b><a href="https://www.youtube.com/watch?v=rzgKDdFTEmQ">Video 📺</a></b>`,
   }, {
     slug: 'mozfest17',
     title: 'MozFest 2017 - London',
@@ -30,8 +31,16 @@ const ME = {
     title: 'Demuxed 2018 - San Francisco',
     img: 'https://archive.org/download/third-eye/third-eye.png',
     descrip: 'Third Eye - tweeting TV text overlays.  CNN MSNBC FOXNEWS BBCNEWS.  (chyrons / lower thirds)',
+  }, {
+    href: 'https://www.youtube.com/watch?v=0gVOlDDg4z4',
+    title: 'Demuxed 2016 - San Francisco',
+    img: 'popcorn.jpg',
+    descrip: `Tracey Jaquith - Popcorn Reborn! Open Video Editor from Mozilla
+      <br><b><a href="https://docs.google.com/presentation/d/1UfyTmXhe39EohAZxS_lktnqspF5uwV4_QsQsT7AoRbs/edit?ts=57fe4fc0#slide=id.g1840168ce8_2_0">Slides 📰</a></b>`,
   },
   ],
+  // https://github.com/traceypooh/textAV
+  // https://www.slideshare.net/tracey_pooh/presentations
 }
 
 /**
@@ -97,13 +106,14 @@ function markup() {
 <div class="list">`
 
     for (const card of list) {
+      const href = card.href || `${SLIDES}${card.slug}`
       str += `
         <div class="card card-body bg-light">
-          <a href="${SLIDES}${card.slug}">
+          <a href="${href}">
             <img src="${card.img}"/>
           </a>
           <h3>
-            <a href="${SLIDES}${card.slug}">${card.title}</a>
+            <a href="${href}">${card.title}</a>
           </h3>
           ${card.descrip || ''}
         </div>`
